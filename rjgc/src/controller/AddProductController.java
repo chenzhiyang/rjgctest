15529f88489732b53d63ceb4bd7fb0511043c5a4
Allan <mac@192.168.0.103>
2017-05-02 09:55:27 +0800

    test111

--- rjgc/src/controller/AddProductController.java
+++ rjgc/src/controller/AddProductController.java
@@ -15,6 +15,8 @@ public class AddProductController extends HttpServlet {
 	public void doPost(HttpServletRequest request,HttpServletResponse response)
 			throws ServletException,IOException {
 		response.setContentType("text/html;charset=utf-8");
+		
+		int i;
 		//接收所有的，用户输入的，商品信息。保存到ProductModel product对象中
 		//调用ProductInfoService的saveProduct(ProductModel product)方法添加商品
 		//如果添加错误，即返回值为false，则跳转到supplierAddProduct.jsp界面，提示用户错误并修改
